7420e580ed330d1b872e7c7022ef4113ee069852
Sa9i9f <58704805+Sa9i9f@users.noreply.github.com>
2021-12-01 18:29:27 +0300

    Saif Faisal - task solved

---
+++ src/Pages/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Drawer from "../components/Drawer";
+
+
+function Item(props) {
+    return ( 
+
+        <div style={{padding:"400px"}}>
+        Cart Page, Loading..
+          </div>
+     );
+}
+
+export default Item;
