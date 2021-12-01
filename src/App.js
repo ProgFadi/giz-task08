887c1cf2340ebb92c687b3066c75a480ed4b69d3
Esraa <esraayaareb@gmail.com>
2021-12-01 23:07:42 +0300

    create 3 empty pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import PersistentDrawerLeft from './components/PersistentDrawerLeft';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Categories from './pages/Categories';
+import Products from './pages/Products';
 
 function App() {
   return (
     <div className="App">
-      <PersistentDrawerLeft />
+      <Routes>
+        <Route path="" element={<PersistentDrawerLeft />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="products" element={<Products />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
