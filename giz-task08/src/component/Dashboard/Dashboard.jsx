8c5bf8be9eb5cecc2ff67f0e931274f3011401c0
mohammadmlaik92 <55026417+mohammadmlaik92@users.noreply.github.com>
2021-12-03 18:11:18 +0300

    Task-08

---
+++ giz-task08/src/component/Dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Typography from "../drawer";
+
+class Dashboard extends Component {
+    render() {
+        return (
+          <Typography>
+              <div style={{padding:'150px'}}>
+                  Dashboard
+              </div>
+
+          </Typography>
+        );
+    }
+}
+
+export default Dashboard;
