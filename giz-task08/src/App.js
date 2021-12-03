a648c3ca87270955a87914ca77a809095048fad2
Yusra_hub <yusraahmed055@gmail.com>
2021-12-03 20:55:56 +0300

    Done

--- giz-task08/src/App.js
+++ giz-task08/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import AppBar from './Components/AppBar';
+import Categories from './Components/Categories';
 import Content from './Components/Content';
 import Home from './Components/Home';
+import Products from './Components/Products/products';
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/appbar" element={<AppBar/>}/>
+      <Route path="/categories" element={<Categories/>}/>
       <Route path="/content" element={<Content/>}/>
+      <Route path="/products" element={<Products/>}/>
+
     </Routes>
     </BrowserRouter>
   );
--- giz-task08/src/Components/AppBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Menu from './Menu'
-export default function AppBar(props) {
-    return (
-        <Menu>
-            This is app bar
-        </Menu>
-    )
-}
