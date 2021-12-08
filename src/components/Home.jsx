df6d804deb19638d89f4853cf94cf3fbb459023e
Zahraa Basheer <68440929+zhr9797@users.noreply.github.com>
2021-12-08 21:35:43 +0300

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Drawer from "./Drawer"
+export default class Home extends Component {
+    render() {
+        return (
+            <Drawer>
+                <h1>Home</h1>
+            </Drawer>
+        )
+    }
+}
